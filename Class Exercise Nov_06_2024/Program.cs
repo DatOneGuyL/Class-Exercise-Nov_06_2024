d880c4312a7ccf48bc35929066ea4959f0ddbf54
DatOneGuy . <tqdDatOneGuy@gmail.com>
2024-11-25 13:21:31 +0700

    Add project files.

---
+++ Class Exercise Nov_06_2024/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter numbers: ");
+        string InitialInput = Console.ReadLine();
+        string[] InputStringArray = InitialInput.Split(' ');
+        int[] IntArr = Array.ConvertAll(InputStringArray, int.Parse);
+
+        for (int i = 1; i < IntArr.Length; i++)
+        {
+            int key = IntArr[i];
+            int j = i - 1;
+
+            while (j >= 0 && IntArr[j] > key)
+            {
+                IntArr[j + 1] = IntArr[j];
+                j = j - 1;
+            }
+            IntArr[j + 1] = key;
+        }
+
+        foreach (int num in IntArr)
+        {
+            Console.Write($"{num} ");
+        }
+    }
+}
